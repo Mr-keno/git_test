1e4e7e5c6e1b9e10db8568c25c6f64467441bb64
nayhtet <nayhtet.nyeinchan.2199@gmail.com>
2024-02-05 14:46:37 +0630

    change mainEntry.java

--- src/main/MainEntry.java
+++ src/main/MainEntry.java
@@ -4,5 +4,6 @@ public class MainEntry {
 	public static void main(String[] args) {
 		System.out.println("Original State");
 		System.out.println("First Changed");
+		System.out.println("Local Repository changed");
 	}
 }
